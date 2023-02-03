c9d918a7eb29bdce08c1f4cdb0842670632dbe01
Peter Hagenson <phagenso@travelers.com>
2023-02-03 09:39:45 -0500

    fetch returning people from swapi api

--- swapi_group3/src/components/Characters.js
+++ swapi_group3/src/components/Characters.js
@@ -1,9 +1,29 @@
-
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 
 function Characters(){
+
+    let [people, setPeople] = useState([])
+
+    async function getPeople(){
+        await fetch('http://swapi.dev/api/people')
+        .then((response) => response.json())
+        .then((returnedPeople) => {
+            console.log(returnedPeople.results)
+           // setFilms(returnedFilms.results)
+        })
+    }
+
+    useEffect(()=>{
+        getPeople()
+    },[])
+
     return (
+        <>
         <h1>Star Wars Universe Lookup</h1>
+        <h3>Who are you looking for? </h3>
+        </>
     )
 }
 
