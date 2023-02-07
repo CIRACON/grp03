b05a97b8d1e83f2ae711e51e728774f82e4ca44c
Peter Hagenson <phagenso@travelers.com>
2023-02-07 11:46:23 -0500

    fetching all people and setting search term

--- swapi_group3/src/components/Characters.js
+++ swapi_group3/src/components/Characters.js
@@ -5,26 +5,40 @@ import {Navigate, useNavigate} from 'react-router-dom'
 function Characters(){
  
     let navigate = useNavigate()
+    let [searchTerm, setSearchTerm] = useState('')
+    let [people, setPeople] = useState([])
     
+    function handleSubmit(event) {
+      event.preventDefault()
+      console.log(searchTerm)
+    }
 
-    // async function getPeople(){
-    //     await fetch('http://localhost:5000/people', {
-    //         method: 'GET',    
-    //         withCredentials: true,    
-    //         crossorigin: true
-    //     })
-    //     .then((response) => response.json())
-    //     .then((p) => {
-    //         console.log(p)
-    //         setPeople(p)
-    //     })
-    // }
+     async function getPeople(){
+        await fetch('http://localhost:5000/people', {
+             method: 'GET',    
+         withCredentials: true,    
+             crossorigin: true
+         })
+         .then((response) => response.json())
+         .then((p) => {
+             console.log(p)
+             setPeople(p)
+         })
+     }
+
+     useEffect(() => {
+      getPeople()
+     }, []) 
 
 
     return (
       <>
         <h1>Star Wars Universe Lookup</h1>
         <h3>What are you looking for?</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}></input>
+        <button type="submit">Search</button>
+        </form>
         <button onClick={()=>navigate('/films')}>Click for Films</button> 
         <button onClick={()=>navigate('/planets')}>Click for Planets</button>
 
