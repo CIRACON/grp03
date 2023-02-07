3cd0fe7ab32f4e18566ae6eb02ab129d19ec2d61
Peter Hagenson <phagenso@travelers.com>
2023-02-07 13:35:24 -0500

    searchTerm filter working, need to conditioanlly render

--- swapi_group3/src/components/Characters.js
+++ swapi_group3/src/components/Characters.js
@@ -6,17 +6,27 @@ function Characters(){
  
     let navigate = useNavigate()
     let [searchTerm, setSearchTerm] = useState('')
-    let [people, setPeople] = useState([])
+    let [people, setPeople] = useState([]) 
+    let [searchPeople, setSearchPeople] = useState([])
     
     function handleSubmit(event) {
       event.preventDefault()
       console.log(searchTerm)
+      filterPeople(people, searchTerm)
     }
 
     function handleClick(person) {
       let id = person.pk
       navigate(`/people/${id}`)
     }
+
+    function filterPeople(arr, query) {
+      console.log(arr)
+      let results = arr.filter((el) => el.fields.name.toLowerCase().includes(query.toLowerCase()));
+    setSearchPeople(results)
+    }
+
+    
 
      async function getPeople(){
         await fetch('http://localhost:5000/people', {
@@ -33,7 +43,8 @@ function Characters(){
 
      useEffect(() => {
       getPeople()
-     }, []) 
+      filterPeople(people, searchTerm)
+     }, [searchTerm]) 
 
 
     return (
@@ -44,6 +55,10 @@ function Characters(){
           <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}></input>
         <button type="submit">Search</button>
         </form>
+        
+        {searchPeople.map((person)=>
+        <p>{person.fields.name}</p>)}
+        
         {people.map((person) =>
         <p onClick={()=>handleClick(person)}>{person?.fields?.name}</p>)}
 
